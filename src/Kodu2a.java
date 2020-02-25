40e08e39a1c1f912419dfa3474c886d772d3e760
georgdemidov <georgbarca@gmail.com>
2020-02-25 11:23:35 +0200

    Update

--- src/Kodu2a.java
+++ src/Kodu2a.java
@@ -1,11 +1,24 @@
+/*****************************************************************************
+ * Programmeerimisharjutused. LTAT.03.007
+ * 2019/2020 kevadsemester
+ *
+ * Kodutöö. Ülesanne nr 2a
+ * Teema:
+ * Üksikul saarel elava eraku kingavaliku simulatsioon.
+ *
+ * Autor: Georg Demidov
+ *
+ *****************************************************************************/
+
 public class Kodu2a {
+
     //Koostan meetodi, mis simuleerib mündiviset oletades, et tahame saada ainult kulli. Kui programm tagastab false, tuli kiri, kui true, siis tuli kull.
     static boolean kull(){
         //Juhul, kui Math.random on alla 0.5, siis tagastatav väärtus on true, vastasel juhul (0.5-st 1-ni) tagastab false.
         return Math.random() < 0.5;
     }
 
-    static int paljajaluVäljumisteArv(int väljumisiKokku){
+    static void paljajaluVäljumisteArv(int väljumisiKokku){
         //Tähistan mündiviske puhul, et kui tuleb kull, siis erak väljub ja siseneb vasakust uksest.
         //Täisarvuline muutuja "vasakUks" algväärtus on kolm, sest selle ukse ees asetseb alguses 3 kinga paari.
         int vasakUks = 3;
@@ -15,38 +28,40 @@ public class Kodu2a {
         //Koostan muutuja "paljadJalad", mis muutub iga kord ühe võrra suuremaks, kui toimub paljajalu väljumine.
         int paljadJalad = 0;
         //Tsüklit rakendades korrutan väljumisiKokku kahega, sest peab ka arvestama, et iga väljumise korral, peab ka tagasi sisenema.
-        for (int i = 0; i < väljumisiKokku * 2; i++) {
-            if (i % 2 == 0) {
-                if (kull()) {
-                    if (vasakUks == 0) {
-                        paljadJalad++;
+        for (int j = 100; j <= väljumisiKokku ; j+= 100) {
+            for (int i = 0; i < 200; i++) {
+                if (i % 2 == 0) {
+                    if (kull()) {
+                        if (vasakUks == 0) {
+                            paljadJalad++;
+                        }else{
+                            vasakUks--;
+                        }
                     }else{
-                        vasakUks--;
+                        if (paremUks == 0) {
+                            paljadJalad++;
+                        }else{
+                            paremUks--;
+                        }
                     }
                 }else{
-                    if (paremUks == 0) {
-                        paljadJalad++;
+                    if (kull()) {
+                        if (vasakUks == 6 || paremUks == 6) {
+                            ;
+                        }else{
+                            vasakUks++;
+                        }
                     }else{
-                        paremUks--;
-                    }
-                }
-            }else{
-                if (kull()) {
-                    if (vasakUks == 6 || paremUks == 6) {
-                        ;
-                    }else{
-                        vasakUks++;
-                    }
-                }else{
-                    if (vasakUks == 6 || paremUks == 6) {
-                        ;
-                    }else{
-                        paremUks++;
+                        if (vasakUks == 6 || paremUks == 6) {
+                            ;
+                        }else{
+                            paremUks++;
+                        }
                     }
                 }
             }
+            vormistus(j, paljadJalad);
         }
-        return paljadJalad;
     }
 
     static double paljajaluVäljumisteProtsent(int väljumisiKokku, int paljajaluVäljumised){
@@ -54,18 +69,17 @@ public class Kodu2a {
         return protsent;
     }
 
+    static void vormistus(int väljumisiKokku, int paljadJalad){
+        double protsent = paljajaluVäljumisteProtsent(väljumisiKokku, paljadJalad);
+        System.out.printf("%7s %13s %12.2f%% %n", väljumisiKokku, paljadJalad, protsent);
+    }
+
     public static void main(String[] args){
         System.out.println("Kodutöö nr 2a.                          Programmi väljund");
         System.out.println("=========================================================:" + "\n");
         System.out.println("Väljumisi    Paljajalu    Paljajalu");
         System.out.println("    kokku    väljumisi    väljumiste %");
-        for (int i = 100; i <= 2000; i += 100) {
-            int väljumisiKokku = i;
-            int paljadJalad = paljajaluVäljumisteArv(väljumisiKokku);
-            double protsent = paljajaluVäljumisteProtsent(väljumisiKokku, paljadJalad);
-            System.out.printf("%7s %13s %12.2f%% %n", väljumisiKokku, paljadJalad, protsent);
-
-        }
+        paljajaluVäljumisteArv(2000);
         System.out.println("\n" + "=========================================================.");
         System.out.println("Georg Demidov                     " + new java.sql.Timestamp(System.currentTimeMillis()));
     }
