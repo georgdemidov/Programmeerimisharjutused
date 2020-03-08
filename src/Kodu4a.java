4427c9b0a2e98253cd7481f2505e5c7a651b5e63
Georg Demidov <georgbarca@gmail.com>
2020-03-08 23:46:47 +0200

    Kodu3a

--- src/Kodu4a.java
+++ src/Kodu4a.java
@@ -10,17 +10,21 @@
  *
  *****************************************************************************/
 
+//Lisan antud liidesed, sest need on vajalikud, et programm töötaks.
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.ListIterator;
-
 import static java.lang.Math.abs;
 
 public class Kodu4a {
-
+    /**
+     * Antud meetod tagastab 12 korda 4 maatriksi, kus on i-ndas reas iga i-nda kuu keskmine temperatuur, suurim aasta temperatuur, vähim aasta temperatuur ning suurima ja vähima temperatuuri vahe.
+     * @param fail - Argumendiks annan meetodile sõnena lähteandmete faili kasutatee.
+     * @return - Meetod tagastab sõnede maatriksi.
+     * @throws IOException - Lisan meetodile juurde selle täpsustuse, et meetod saaks aru, juhul kui faili avamisega on probleeme, siis see viskab ette vea, mis näitab, et tekib probleem faili avamisega.
+     */
     static String[][] temperatuuriAndmeteMaatriks(String fail) throws IOException{
         String[][] maatriks = new String[12][5];
         double temperatuurideSumma = 0;
@@ -31,7 +35,6 @@ public class Kodu4a {
         int i = 0;
         int kuu = 1;
         int päevadeArv = 0;
-        String [] kuud = {"jaanuar", "veebruar", "märts", "aprill", "mai", "juuni", "juuli", "august", "september", "oktoober", "november", "detsember"};
         BufferedReader lugeja = new BufferedReader(new FileReader(fail));
         ArrayList<String> temperatuurid = new ArrayList<>();
         while (true) {
@@ -61,11 +64,10 @@ public class Kodu4a {
             }else{
                 vahe = suurim - vähim;
                 keskmineTemperatuur = temperatuurideSumma / (päevadeArv);
-                maatriks[i][0] = kuud[i];
-                maatriks[i][1] = String.valueOf(keskmineTemperatuur);
-                maatriks[i][2] = String.valueOf(suurim);
-                maatriks[i][3] = String.valueOf(vähim);
-                maatriks[i][4] = String.valueOf(vahe);
+                maatriks[i][0] = String.valueOf(keskmineTemperatuur);
+                maatriks[i][1] = String.valueOf(suurim);
+                maatriks[i][2] = String.valueOf(vähim);
+                maatriks[i][3] = String.valueOf(vahe);
                 i++;
                 if (kuu < 9){
                     kuu++;
@@ -74,23 +76,24 @@ public class Kodu4a {
                 }
                 päevadeArv = 0;
                 temperatuurideSumma = 0;
+                temperatuurideSumma += Double.parseDouble(osad[2]);
                 suurim = Integer.MIN_VALUE;
                 vähim = Integer.MAX_VALUE;
             }
         }
         vahe = suurim - vähim;
         keskmineTemperatuur = temperatuurideSumma/ (päevadeArv);
-        maatriks[i][0] = kuud[i];
-        maatriks[i][1] = String.valueOf(keskmineTemperatuur);
-        maatriks[i][2] = String.valueOf(suurim);
-        maatriks[i][3] = String.valueOf(vähim);
-        maatriks[i][4] = String.valueOf(vahe);
+        maatriks[i][0] = String.valueOf(keskmineTemperatuur);
+        maatriks[i][1] = String.valueOf(suurim);
+        maatriks[i][2] = String.valueOf(vähim);
+        maatriks[i][3] = String.valueOf(vahe);
         return  maatriks;
     }
 
     static void vormistus(String[][] maatriks){
+        String [] kuud = {"jaanuar", "veebruar", "märts", "aprill", "mai", "juuni", "juuli", "august", "september", "oktoober", "november", "detsember"};
         for (int i = 0; i < 12; i++) {
-            System.out.printf("%10s %10.3f %10.3f %10.3f %10.3f", maatriks[i][0], Double.parseDouble(maatriks[i][1]), Double.parseDouble(maatriks[i][2]), Double.parseDouble(maatriks[i][3]), Double.parseDouble(maatriks[i][4]));
+            System.out.printf("%10s %10.3f %10.3f %10.3f %10.3f", kuud[i], Double.parseDouble(maatriks[i][0]), Double.parseDouble(maatriks[i][1]), Double.parseDouble(maatriks[i][2]), Double.parseDouble(maatriks[i][3]));
             System.out.println();
         }
     }
@@ -98,7 +101,7 @@ public class Kodu4a {
     static double keskmisteKeskmine(String[][] maatriks){
         double keskmisteKeskmine = 0;
         for (int i = 0; i < 12; i++) {
-            keskmisteKeskmine += Double.parseDouble(maatriks[i][1]);
+            keskmisteKeskmine += Double.parseDouble(maatriks[i][0]);
         }
         return keskmisteKeskmine / 12;
     }
@@ -107,10 +110,11 @@ public class Kodu4a {
         double keskmine = keskmisteKeskmine(maatriks);
         String lähimkuu = "tere";
         double vahe = Integer.MAX_VALUE;
+        String [] kuud = {"jaanuar", "veebruar", "märts", "aprill", "mai", "juuni", "juuli", "august", "september", "oktoober", "november", "detsember"};
         for (int i = 0; i < 12; i++) {
-            if (abs(keskmine - Double.parseDouble(maatriks[i][1])) < vahe){
-                vahe = keskmine - Double.parseDouble(maatriks[i][1]);
-                lähimkuu = maatriks[i][0];
+            if (abs(keskmine - Double.parseDouble(maatriks[i][0])) < vahe){
+                vahe = keskmine - Double.parseDouble(maatriks[i][0]);
+                lähimkuu = kuud[i];
             }
         }
         return lähimkuu;
@@ -120,10 +124,14 @@ public class Kodu4a {
         System.out.println("Kodutöö nr 4a.                          Programmi väljund");
         System.out.println("=========================================================:" + "\n");
         System.out.println();
-        System.out.println("       AASTA 2018 TEMPERATUURIDE TABEL KUUDE LÕIKES");
+        String fail = "C:\\Users\\demidov\\IdeaProjects\\Programmeerimisharjutused\\src\\temperatuurid2018.txt";
+        if (Character.getNumericValue(fail.charAt(76)) == 8){
+            System.out.println("       AASTA 2018 TEMPERATUURIDE TABEL KUUDE LÕIKES");
+        }else{
+            System.out.println("       AASTA 2019 TEMPERATUURIDE TABEL KUUDE LÕIKES");
+        }
         System.out.println("    E-ilmajaama (Tartus füüsikahoone katusel) andmetel\n");
         System.out.println("             keskmine     suurim      vähim       vahe");
-        String fail = "src/temperatuurid2018.txt";
         vormistus(temperatuuriAndmeteMaatriks(fail));
         System.out.println();
         System.out.println("-------------------------------------------------------");
